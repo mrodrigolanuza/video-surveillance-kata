b4c45d0eb63865c66402dd7456275124d21b725a
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-08 19:22:50 +0200

    Refactorización: Usar jest.spyOn para validar que se llama a los métodos FakeRecorder cuando corresponde

--- src/tests/surveillanceController.test.ts
+++ src/tests/surveillanceController.test.ts
@@ -3,22 +3,24 @@ import { MotionSensor, VideoRecorder, SurveillanceController } from "../core/sur
 describe("The Surveillance Controller", ()=>{
     it("asks the recorder to stop when the sensor detects no motion", ()=>{
         const motionSensor = new StubSensorNoDetectingMotion();
-        const recorder = new SpyRecorder();
+        const recorder = new FakeRecorder();
         const controller = new SurveillanceController(motionSensor, recorder);
+        const spyRecorder = jest.spyOn(recorder, 'stopRecording')
         
         controller.recordMotion();
 
-        expect(recorder.stopRecordingCalled).toBeTruthy();
+        expect(spyRecorder).toHaveBeenCalled();
     });
 
     it("asks the recorder to start when the sensor detects motion", ()=>{
         const motionSensor = new StubSensorDetectingMotion();
-        const recorder = new SpyRecorder();
+        const recorder = new FakeRecorder();
         const controller = new SurveillanceController(motionSensor, recorder);
-        
+        const spyRecorder = jest.spyOn(recorder, 'startRecording')
+
         controller.recordMotion();
 
-        expect(recorder.startRecordingCalled).toBeTruthy();
+        expect(spyRecorder).toHaveBeenCalled();
     });
 });
 
@@ -36,7 +38,7 @@ class StubSensorNoDetectingMotion implements MotionSensor{
 }
 
 //Creación de objeto Fake que simula (o hace de doble) de la cámara de grabación
-class SpyRecorder implements VideoRecorder{
+class FakeRecorder implements VideoRecorder{
     startRecordingCalled = false;
     stopRecordingCalled = false;
     startRecording(): void {
